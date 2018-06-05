3303a5732774f4733b415f7c03b97130d4a75781
Alexander Bantyev <balsoft@yandex.ru>
2018-06-05 22:37:28 +0300

    Fixed links

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,258 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.	Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+	# ========================== HARDWARE =====================================
+	imports = [
+		./hardware-configuration.nix
+        "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+	];
+	
+	hardware.cpu.amd.updateMicrocode = true;
+	hardware.opengl.enable = true;
+	hardware.opengl.driSupport32Bit = true;
+	# =========================================================================
+	
+	
+	
+	# ============================ BOOT =======================================
+	boot = {
+		loader = {
+			grub = {
+				enable = true;
+				version = 2;
+				efiSupport = true;
+				useOSProber = true;
+				device = "nodev";
+			};
+			systemd-boot.enable = true;
+			efi = {
+				canTouchEfiVariables = true;
+				efiSysMountPoint = "/boot";
+			};
+		};
+		consoleLogLevel = 0;
+		kernelPackages = pkgs.linuxPackages_latest;
+		kernelParams = ["scsi_mod.use_blk_mq=1"];
+		extraModprobeConfig = ''
+options iwlwifi bt_coex_active=0
+		'';
+	};
+	# =========================================================================
+	
+	
+	
+	# ======================== FILESYSTEMS ====================================
+	fileSystems = {
+		"/".device = "/dev/sda5";
+		"/home/".device = "/dev/sda4";
+		"/boot".device = "/dev/sda1";
+		"/run/media/ubuntu".device = "/dev/sda2";
+	};
+	
+	services.udev.extraRules = ''
+		ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*", ATTR{queue/scheduler}="bfq"
+	'';
+
+	swapDevices = [
+		{
+		    device = "/dev/sda3"; 
+		}
+	];
+	
+	security.pam.enableEcryptfs = true; # For /home
+	# =========================================================================
+	
+	
+	
+	# ====================== TIME & LOCALE ====================================
+	# Set your time zone.
+	time.timeZone = "Europe/Moscow";
+	# =========================================================================
+	
+	
+	
+	# ====================== NETWORKING =======================================
+	networking = {
+		networkmanager.enable = true;
+		hostName = "HP-Laptop";
+		firewall.enable = false;
+	};
+	# =========================================================================
+
+	
+	
+	# ===================== GRAPHICS & FONTS ==================================
+	services.xserver = {
+        enable = true;
+		videoDrivers = [ "amdgpu" ];
+		libinput = {
+			enable = true;
+			sendEventsMode = "disabled-on-external-mouse";
+			middleEmulation = false;
+		};
+		#desktopManager.plasma5.enable = true;
+		desktopManager.gnome3.enable = true;
+		desktopManager.mate.enable = true;
+		#displayManager.lightdm.enable = true;
+		displayManager.slim = {
+			enable = false;
+#			#autoLogin = true;
+#			defaultUser = "balsoft";
+		};
+		displayManager.job.execCmd = "";
+	};
+	programs.bash.shellInit = ''
+		if [[ `tty` = /dev/tty2 ]]
+		then
+			while true
+			do
+				echo "============================== STARTING PLASMA 5 ================================="
+				dbus-launch startplasmacompositor
+				echo "================================ RESTARTING ======================================"
+			done
+
+		fi
+		if [[ `tty` = /dev/tty1 ]]
+		then
+			while true
+			do	
+				echo "=========================== STARTING GNOME SHELL ================================="
+				/home/balsoft/run_gnome_session.nixsh
+				echo "=========================== EXITING GNOME SHELL =================================="
+			done
+		fi
+	'';
+	fonts = {
+		fonts = with pkgs; 
+		[
+			terminus_font
+			opensans-ttf
+			roboto
+			roboto-mono
+			roboto-slab
+			powerline-fonts
+		];
+		enableDefaultFonts = true;
+	};
+	# =========================================================================
+
+	
+	
+	# ====================== SOUND ============================================
+	hardware.pulseaudio = {
+		enable = true;
+		package = pkgs.pulseaudioFull;
+		support32Bit = true;
+	};
+	
+	hardware.bluetooth = {
+		enable = true;
+	};
+	# =========================================================================
+	
+	
+	
+	# ====================== PROGRAMS & SERVICES ==============================
+	virtualisation.virtualbox.host.enable = true;
+	
+	system.autoUpgrade = {
+		dates = "19:00";
+		enable = true; 	
+	};
+	
+	services.openssh.enable = true;
+	services.postgresql = {
+		enable = true;
+		authentication = lib.mkForce ''
+		    # Generated file; do not edit!
+		    # TYPE  DATABASE        USER            ADDRESS                 METHOD
+		    local   all             all                                     trust
+		    host    all             all             127.0.0.1/32            trust
+		    host    all             all             ::1/128                 trust
+		'';
+	};
+	services.printing = {
+		enable = true;
+		drivers = [ pkgs.gutenprint ];
+	};
+	
+	
+	services.dbus.packages = [
+		pkgs.gnome3.gconf
+	];
+
+	services.gnome3 = {
+		chrome-gnome-shell.enable = true;
+		gpaste.enable = true;
+		gvfs.enable = true;
+		gnome-online-accounts.enable = true;
+	};
+
+	services.tor = {
+		enable = true;
+		client.enable = true;
+		client.privoxy.enable = true;
+		torsocks.enable = true;
+	};
+
+	services.avahi.enable = true;
+	programs.adb.enable = true;
+
+	systemd.services.gdrive = {
+		enable = true;
+		requires = ["network-online.target"];
+		description = ''
+			Google Drive user service
+		'';
+		wantedBy = ["multi-user.target"];
+		serviceConfig = {
+			User = "balsoft";
+			ExecStart = ''
+				${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse -debug "/home/balsoft/Google Drive/"
+			'';
+		};
+	};
+	systemd.services.fix_touchpad = {
+        description = "Fix touchpad bug";
+        after = ["suspend.target"];
+        wantedBy = ["suspend.target"];
+        script = ''
+			echo "Fixing touchpad on `date`"
+            /run/current-system/sw/bin/modprobe -r psmouse
+            /run/current-system/sw/bin/modprobe psmouse
+			echo "Done fixing touchpad"
+        '';
+	};
+	environment.variables = {
+		QT_STYLE_OVERRIDE = "kvantum";
+	};
+	services.packagekit.enable = true;
+	hardware.sensor.iio.enable = true;
+	i18n = {
+		defaultLocale = "en_GB.UTF-8";
+	};
+	# =========================================================================
+	
+	
+	
+	# ======================= USERS & SECURITY ================================
+	security.apparmor.enable = true;
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	nixpkgs.config.allowUnfree = true;
+	users.mutableUsers = false;
+	users.extraUsers.balsoft = {
+		isNormalUser = true;
+		extraGroups = ["sudo" "wheel" "networkmanager" "disk" "sound" "pulse" "adbusers" "input"];
+		description = "Александр Бантьев";
+		uid = 1000;
+		password = "";
+	};
+	# =========================================================================
+	
+	# The NixOS release to be compatible with for stateful data such as databases.
+	system.stateVersion = "18.03";
+}
--- home.nix
+++ home.nix
@@ -1,1 +1,159 @@
-/home/balsoft/.config/nixpkgs/home.nix
+
+{pkgs, ...}:
+{
+	programs.home-manager = {
+		enable = true;
+		path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+	};
+	
+	programs.zsh = {
+		enable = true;
+		enableAutosuggestions = true;
+		enableCompletion = true;
+		oh-my-zsh = 
+		{
+			enable = true;
+			theme = "agnoster";
+			plugins = 
+			[
+				"git"
+				"compleat"
+				"dirhistory"
+			];
+		};
+		initExtra = ''
+			#randomquote(){${pkgs.curl}/bin/curl -s $(${pkgs.curl}/bin/curl -s "http://bash.im/forweb/"|grep -o "http://bash.im/quote/[[:digit:]]*") | iconv -f CP1251 -t UTF-8 | ${pkgs.gnugrep}/bin/grep "<div class=\"text\">[^<]*" -a|${pkgs.gnused}/bin/sed "s/<br>/\x0a/g" | ${pkgs.gnused}/bin/sed "s/<br \/>/\x0a/"|${pkgs.gnused}/bin/sed "s/<div class=\x22text\x22>//"|${pkgs.gnused}/bin/sed "s/<\/div>//"|${pkgs.gnused}/bin/sed "s/&quot;/\"/g" |${pkgs.gnused}/bin/sed "s/&gt;/>/g"| ${pkgs.gnused}/bin/sed "s/&lt;/</g"| ${pkgs.gnused}/bin/sed 's/^\s*//g'}
+			# commands to ignore
+			cmdignore=(htop tmux top vim)
+			function active_window_id () {
+				if [[ -n $DISPLAY ]] ; then
+				    xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}'
+				    return
+				fi
+				echo nowindowid
+			}
+
+			# end and compare timer, notify-send if needed
+			function notifyosd-precmd() {
+				retval=$?
+				if [ ! -z "$cmd" ]; then
+				    cmd_end=`date +%s`
+				    ((cmd_time=$cmd_end - $cmd_start))
+				fi
+				if [ $retval -eq 0 ]; then
+				    cmdstat="succesfully"
+				    sound="/home/balsoft/.local/share/gnome-shell/extensions/timepp@zagortenay333/data/sounds/bell.ogg"
+				else
+				    cmdstat="with errors"
+				    sound="/home/balsoft/.local/share/gnome-shell/extensions/timepp@zagortenay333/data/sounds/goat.ogg"
+				fi
+				if [ ! -z "$cmd" -a $cmd_time -gt 10 -a ! $term_window = $(active_window_id) ]; then
+				    if [ ! -z $SSH_TTY ] ; then
+					${pkgs.libnotify}/bin/notify-send -i utilities-terminal -u low "$cmd on `hostname` completed $cmdstat" "\"$cmd\" took $cmd_time seconds"
+				    else
+					${pkgs.libnotify}/bin/notify-send -i utilities-terminal -u low ""$cmd" completed $cmdstat" "\"$cmd\" took $cmd_time seconds"
+				    fi	
+				    ${pkgs.gnome3.gsound}/bin/gsound-play -f $sound
+				fi
+				unset cmd
+			}
+
+			# make sure this plays nicely with any existing precmd
+			precmd_functions+=( notifyosd-precmd )
+
+			# get command name and start the timer
+			function notifyosd-preexec() {
+			    cmd=$1
+			    term_window=$(active_window_id)
+			    cmd_start=`date +%s`
+			}
+
+			# make sure this plays nicely with any existing preexec
+			preexec_functions+=( notifyosd-preexec )
+			XDG_DATA_DIRS=$XDG_DATA_DIRS:$GSETTINGS_SCHEMAS_PATH
+			export QT_STYLE_OVERRIDE=kvantum
+		'';
+	};
+
+
+
+	gtk = {
+		enable = true;
+		iconTheme = 
+		{
+			name = "Papirus-Dark";
+			package = pkgs.papirus-icon-theme;
+		};
+		theme = 
+		{
+			name = "Adapta-Nokto-Teal";
+			package = pkgs.adapta-gtk-theme;
+		};
+		
+	};
+
+	home.packages = with pkgs; [
+		# Internet
+		wget
+		curl
+		chromium
+		gnome3.epiphany
+		# IDE
+		vscode
+		geany
+		kdevelop
+		jetbrains.pycharm-community
+		# Messaging
+		discord
+		tdesktop
+		telepathy_haze
+		telepathy_idle
+		libnotify
+		gnome3.evolution
+		skype
+		# Audio/Video
+		vlc
+		kdenlive
+		google-play-music-desktop-player
+		# Tools
+		zip
+		unrar
+		mc
+		gnome3.gnome-terminal
+		tilix
+		gnome3.gnome-tweak-tool
+		transmission
+		qtstyleplugin-kvantum-qt4
+		nox
+		gnome3.gnome-maps
+		gnome3.gnome-clocks
+		pinta
+		adapta-backgrounds
+		gnome3.gnome-weather
+		wine
+		gnome3.gnome-calendar
+	];
+	home.keyboard = {
+		options = ["grp:caps_toggle" "grp_led:caps"];
+	};
+	xdg = {
+		enable = true;
+		configFile."libinput-gestures.conf".text = ''
+gesture swipe up 4 _internal ws_up
+gesture swipe down 4 _internal ws_down
+gesture pinch in 2 dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:Main.overview.toggle();
+gesture pinch out 2 dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:Main.overview.toggle();
+gesture swipe left 4 dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:Main.wm._switchApp();  
+gesture swipe right 4 dbus-send --session --type=method_call --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:Main.wm._switchApp();  
+		'';
+	};
+	home.file = {
+		"run_gnome_session.nixsh".text = ''
+#! /usr/bin/env nix-shell
+#! nix-shell -p glib.dev -i bash
+XDG_SESSION_TYPE=wayland dbus-run-session ${pkgs.gnome3.gnome-session}/bin/gnome-session
+		'';
+	};
+	
+	programs.command-not-found.enable = true;
+}
