df546e0996e5c86e29f0763e07c08810c069f4b3
Александр Бантьев <balsoft@yandex.ru>
2019-02-13 16:46:41 +0300

    Update tg patch

--- modules/packages.nix
+++ modules/packages.nix
@@ -15,7 +15,7 @@ nixpkgs.overlays = [ (self: old: {
       });};
     tdesktop = old.tdesktop.overrideAttrs (oldAttrs: {
         patches = [
-        (builtins.fetchurl { url = "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/1.5.6/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch"; sha256 = "95800293734d894c65059421d7947b3666e3cbe73ce0bd80d357b2c9ebf5b2e5"; })
+        (builtins.fetchurl "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/9999/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch")
         ] ++ oldAttrs.patches;
         });
      
