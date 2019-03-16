6a53ebca55a4a774856a94cc0da35278d8ffaeb7
Александр Бантьев <balsoft@yandex.ru>
2019-03-17 00:20:59 +0300

    Ssh askpass

--- modules/workspace/ssh.nix
+++ modules/workspace/ssh.nix
@@ -4,7 +4,7 @@
     enable = true;
     passwordAuthentication = false;
   };
-  
+  programs.ssh.askPassword = "${pkgs.ksshaskpass}/bin/ksshaskpass";
   users.users.balsoft.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDd2OdcSHUsgezuV+cpFqk9+Svtup6PxIolv1zokVZdqvS8qxLsA/rwYmQgTnuq4/zK/GIxcUCH4OxYlW6Or4M4G7qrDKcLAUrRPWkectqEooWRflZXkfHduMJhzeOAsBdMfYZQ9024GwKr/4yriw2BGa8GbbAnQxiSeTipzvXHoXuRME+/2GsMFAfHFvxzXRG7dNOiLtLaXEjUPUTcw/fffKy55kHtWxMkEvvcdyR53/24fmO3kLVpEuoI+Mp1XFtX3DvRM9ulgfwZUn8/CLhwSLwWX4Xf9iuzVi5vJOJtMOktQj/MwGk4tY/NPe+sIk+nAUKSdVf0y9k9JrJT98S/ comment" ];
   home-manager.users.balsoft.programs.ssh =
   if (! isNull config.secrets.id_rsa) then  {
