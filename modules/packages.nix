b27d5403eb6ea64a83700f617351f0b19cdef73d
Александр Бантьев <balsoft@yandex.ru>
2019-03-22 01:21:29 +0300

    Some more formatting

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,32 +1,31 @@
 {pkgs, config, lib, ...}:
 {
-  nixpkgs.overlays = [ (self: old: 
-    {
-      termNote = (import (builtins.fetchGit {url = "https://github.com/Terodom/termNote"; rev = "a4045a75dca67891ef239a43f364ce3149a91b6a";}) {});
-      
-      tdesktop = old.tdesktop.overrideAttrs (oldAttrs: {
-        patches = [
-          (builtins.fetchurl 
+  nixpkgs.overlays = [ (self: old: {
+    termNote = (import (builtins.fetchGit {url = "https://github.com/Terodom/termNote"; rev = "a4045a75dca67891ef239a43f364ce3149a91b6a";}) {});
+
+    tdesktop = old.tdesktop.overrideAttrs (oldAttrs: {
+      patches = [
+        (builtins.fetchurl 
+          {
+            url = "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/9999/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch"; 
+            sha256 = "423e9ead98358a92ed1970713b39be83c17159a1609074648164e31ba56b1b47";
+          })
+      ] ++ oldAttrs.patches;
+    });
+    pythonPackages = old.pythonPackages.override 
+      {
+        overrides = 
+          (self: super:
             {
-              url = "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/9999/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch"; 
-              sha256 = "423e9ead98358a92ed1970713b39be83c17159a1609074648164e31ba56b1b47";
-            })
-        ] ++ oldAttrs.patches;
-      });
-      pythonPackages = old.pythonPackages.override 
-        {
-          overrides = 
-            (self: super:
-              {
-                backports_functools_lru_cache = super.backports_functools_lru_cache.overrideAttrs (oldAttrs:
-                  oldAttrs // {
-                    meta.priority = 1000;
-                  });
-              });
-        };
-    } // (if config.device == "Prestigio-Laptop" then {
-      grub2 = old.pkgsi686Linux.grub2;
-    } else {}))];
+              backports_functools_lru_cache = super.backports_functools_lru_cache.overrideAttrs (oldAttrs:
+                oldAttrs // {
+                  meta.priority = 1000;
+                });
+            });
+      };
+  } // (if config.device == "Prestigio-Laptop" then {
+    grub2 = old.pkgsi686Linux.grub2;
+  } else {}))];
   nixpkgs.pkgs = import ../imports/nixpkgs
     {
       config.allowUnfree = true;
