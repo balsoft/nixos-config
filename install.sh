233127096d7a4addf6c6b47666967f0c7689bfd1
Александр Бантьев <balsoft@yandex.ru>
2019-03-25 18:05:45 +0300

    Automatic commit. This builds at Пн мар 25 18:05:45 MSK 2019

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p p7zip git -i bash
+
+if [[ ! -e ~/.local/share/password ]]
+then
+    echo -n "Password [echoed]: "
+    read password
+    echo $password > ~/.local/share/password
+fi
+
+7z e secret.nix.zip -y -p`cat ~/.local/share/password`
+
+git submodule update --init --recursive
+
+export NIX_PATH=nixpkgs=./imports/nixpkgs:nixos-config=/etc/nixos/configuration.nix 
+
+nix build -f ./imports/nixpkgs/nixos system &&
+    {
+        git add .
+        git commit -m "Automatic commit. This builds at `date`"
+        git tag latestBuild --force
+        sudo nixos-rebuild switch
+    }
+
