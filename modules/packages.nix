881f1fc2f38f6002ea3147cf8d36d3a35e2e19cb
Александр Бантьев <balsoft@yandex.ru>
2019-03-25 17:55:58 +0300

    Update pkgs

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,38 +1,43 @@
 {pkgs, config, lib, ...}:
 {
-nixpkgs.overlays = [ (self: old: {
-    nur = pkgs.callPackage (import (builtins.fetchGit {
-      url = "https://github.com/nix-community/NUR";
-    })) {};
-    termNote = (import (builtins.fetchGit {url = "https://github.com/Terodom/termNote"; ref = "master";}) {});
+  nixpkgs.overlays = [ (self: old: {
+    termNote = (import (builtins.fetchGit {url = "https://github.com/Terodom/termNote"; rev = "a4045a75dca67891ef239a43f364ce3149a91b6a";}) {});
 
-    movit = old.movit.overrideAttrs (oldAttrs: { # Currently, movit fails
-      doCheck = false;
-      GTEST_DIR = "${old.gtest.src}/googletest";
-    });
-    plasma5 = old.plasma5 // {xdg-desktop-portal-kde = old.plasma5.xdg-desktop-portal-kde.overrideAttrs (oldAttrs: {
-      buildInputs = oldAttrs.buildInputs ++  [ old.cups ];
-      });};
+    lambda-launcher = (import (builtins.fetchGit { url = "https://github.com/balsoft/lambda-launcher"; rev = "acdf578c18a9ee1788fc731028a8bda29b7d9a23";}) {});
+
     tdesktop = old.tdesktop.overrideAttrs (oldAttrs: {
-        patches = [
-        (builtins.fetchurl "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/9999/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch")
-        ] ++ oldAttrs.patches;
-        });
-     
-    } // (if config.device == "Prestigio-Laptop" then {
+      patches = [
+        (builtins.fetchurl 
+          {
+            url = "https://raw.githubusercontent.com/msva/mva-overlay/master/net-im/telegram-desktop/files/patches/9999/conditional/wide-baloons/0001_baloons-follows-text-width-on-adaptive-layout.patch"; 
+            sha256 = "423e9ead98358a92ed1970713b39be83c17159a1609074648164e31ba56b1b47";
+          })
+      ] ++ oldAttrs.patches;
+    });
+    pythonPackages = old.pythonPackages.override 
+      {
+        overrides = 
+          (self: super:
+            {
+              backports_functools_lru_cache = super.backports_functools_lru_cache.overrideAttrs (oldAttrs:
+                oldAttrs // {
+                  meta.priority = 1000;
+                });
+            });
+      };
+  } // (if config.device == "Prestigio-Laptop" then {
     grub2 = old.pkgsi686Linux.grub2;
-    } else {}))];
+  } else {}))];
   nixpkgs.pkgs = import ../imports/nixpkgs
-  {
-    config.allowUnfree = true;
-    config.android_sdk.accept_license = true;
-  } // config.nixpkgs.config;
+    {
+      config.allowUnfree = true;
+      config.android_sdk.accept_license = true;
+    } // config.nixpkgs.config;
+
   nix.nixPath = lib.mkForce
-  [
-  "nixpkgs=${../imports/nixpkgs}"
-  "home-manager=${../imports/home-manager}"
-  "nixos-config=/etc/nixos/configuration.nix"
-  ];  
-  
-  
+    [
+      "nixpkgs=${../imports/nixpkgs}"
+      "home-manager=${../imports/home-manager}"
+      "nixos-config=/etc/nixos/configuration.nix"
+    ];
 }
