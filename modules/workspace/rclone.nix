afa0c921a740947c8cb401e4b6bf85fe19881a84
Александр Бантьев <balsoft@yandex.ru>
2019-04-06 12:41:32 +0300

    Automatic commit. This builds at Сб апр  6 12:41:32 MSK 2019

--- modules/workspace/rclone.nix
+++ modules/workspace/rclone.nix
@@ -5,7 +5,7 @@
     home.activation."rclone" = {
       after = ["linkGeneration"];
       before = [];
-      data = "cp ./.config/rclone/rclone.conf.home ./.config/rclone/rclone.conf; chmod 700 ./.config/rclone/rclone.conf";
+      data = "cp ./.config/rclone/rclone.conf.home ./.config/rclone/rclone.conf; chmod 700 ./.config/rclone/rclone.conf; mkdir -p '~/Google Drive' '~/Yandex Disk' '~/Dropbox'";
     };
     xsession.windowManager.i3.config.startup = [
       { command = "${pkgs.rclone}/bin/rclone mount google:/ '/home/balsoft/Google Drive' --daemon"; }
