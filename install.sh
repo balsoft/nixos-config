4b99513c654ca85371342e4fc335ac309cafb553
Alexander Bantyev <balsoft@yandex.ru>
2019-05-26 19:54:09 +0300

    Automatic commit. This builds at 1558889649

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p p7zip git -i bash
 
+firsttime=false
+
+if [[ $1 == "--first-time" ]]
+then
+    firsttime=true
+    shift
+    hostname=$2
+    shift
+fi
+
 unset IN_NIX_SHELL
 
 if [[ ! -e ~/.local/share/password ]]
@@ -14,17 +24,25 @@ fi
 
 git submodule update --init --recursive
 
-export NIX_PATH=nixpkgs=./imports/nixpkgs:nixos-config=/etc/nixos/configuration.nix 
+export NIX_PATH=nixpkgs=./imports/nixpkgs:nixos-config=/etc/nixos/configuration.nix
 
-nix build -f ./imports/nixpkgs/nixos system $@ &&
-    {
-        git add .
-        d=$(date +%s)
-        git commit -m "Automatic commit. This builds at $d"
-        git tag latestBuild --force
-        dir=$(pwd)
-        export SHELL=/bin/sh 
-        pkexec nix-env --profile /nix/var/nix/profiles/system --set $(readlink $dir/result)
-        pkexec $dir/result/bin/switch-to-configuration switch
-    }
-
+if $firsttime
+then
+    nixos-generate-config --root /mnt
+    echo 'import /home/balsoft/projects/nixos-config "$hostname"' > /mnt/etc/nixos/configuration.nix
+    mount --rbind /mnt/home /home/
+    cp /mnt/etc/nixos/* /etc/nixos
+    nixos-install $@
+else
+    nix build -f ./imports/nixpkgs/nixos system $@ &&
+        {
+            git add .
+            d=$(date +%s)
+            git commit -m "Automatic commit. This builds at $d"
+            git tag latestBuild --force
+            dir=$(pwd)
+            export SHELL=/bin/sh 
+            pkexec nix-env --profile /nix/var/nix/profiles/system --set $(readlink $dir/result)
+            pkexec $dir/result/bin/switch-to-configuration switch
+        }
+fi
