cb4b8883aff757c1e60e9d2a91e0424f76303fe7
Alexander Bantyev <balsoft@yandex.ru>
2019-05-27 08:22:27 +0300

    Fix frequency block

--- modules/workspace/i3blocks/default.nix
+++ modules/workspace/i3blocks/default.nix
@@ -63,11 +63,11 @@ in {
           top -b -n1 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }' '';
       interval = 3;
     };
-    k_cpufreq = pkgs.stdenv.lib.optionalAttrs config.deviceSpecific.isLaptop {
+    k_cpufreq = {
       command = ''
         echo $(${
           pkgs.bc
-        }/bin/bc -l <<< "scale=2; `cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq|sort|head -1`/1000000") GHz'';
+        }/bin/bc -l <<< "scale=2; `cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq|sort|tail -1`/1000000") GHz'';
     };
     l_temperature = pkgs.stdenv.lib.optionalAttrs
     config.deviceSpecific.isLaptop { command = scripts.temperature; };
