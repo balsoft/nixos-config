73053c0fed43b9d3786aa7a724fe36e5f3d21e6a
Alexander Bantyev <balsoft@yandex.ru>
2019-05-27 15:42:48 +0300

    Automatic commit. This builds at 1558960968

--- modules/applications/weechat.nix
+++ modules/applications/weechat.nix
@@ -86,9 +86,9 @@ in {
       python.slack.unfurl_ignore_alt_text = "When displaying ("unfurling") links to channels/users/etc, ignore the "alt text" present in the message and instead use the canonical name of the thing being linked to."
       python.slack.unhide_buffers_with_activity = "When activity occurs on a buffer, unhide it even if it was previously hidden (whether by the user or by the distracting_channels setting)."
     '';
-    nixpkgs.overlays = [
-      (self: super: {
-        weechat = super.weechat.override {
+    xsession.windowManager.i3.config.startup = [{
+      command =
+        "konsole -e ${pkgs.weechat.override {
           configure = {availablePlugins, ...}: 
           {
             /*
@@ -96,14 +96,9 @@ in {
               (python.withPackages (ps: with ps; [ websocket_client websocket]))
             ];
             */
-            scripts = with self.weechatScripts; [wee-slack];
+            scripts = with pkgs.weechatScripts; [wee-slack];
           };
-        };
-      })
-    ];
-    xsession.windowManager.i3.config.startup = [{
-      command =
-        "konsole -e ${pkgs.weechat}/bin/weechat";
+        }}/bin/weechat";
     }];
   };
 }
