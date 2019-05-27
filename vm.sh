eaf90e0abb389cabb9b475063b3a975dbec0fabf
Alexander Bantyev <balsoft@yandex.ru>
2019-05-27 10:02:14 +0300

    Automatic commit. This builds at 1558940534

--- vm.sh
+++ vm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 [[ -z ./secret.nix ]] && echo null > secret.nix
-nixos-rebuild build-vm -I nixpkgs=./imports/nixpkgs -I nixos-config=./vm.nix
+NIX_PATH=nixpkgs=./imports/nixpkgs:nixos-config=./vm.nix nix build -f ./imports/nixpkgs/nixos vm
 ./result/bin/run-NixOS-VM-vm -m size=4G # Sorry, my apps are way too large
 
