39a1f199e60b3216f82c5d3baf06e023353ec4af
Alexander Bantyev <balsoft@yandex.ru>
2019-05-28 12:28:44 +0300

    Automatic commit. This builds at 1559035724

--- modules/workspace/compton.nix
+++ modules/workspace/compton.nix
@@ -3,6 +3,6 @@
     programs.autorandr.hooks.preswitch.compton = "pkill compton";
     programs.autorandr.hooks.postswitch.compton = "allow_rgb10_configs=false ${
       pkgs.compton
-    }/bin/compton --backend xr-glx-hybrid -i 0 --vsync opengl-swc -C --shadow-exclude '!focused' &";
+    }/bin/compton --backend xr-glx-hybrid -i 0 --vsync opengl-swc -cC --shadow-exclude '!focused' &";
   };
 }
