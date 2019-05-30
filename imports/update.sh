5b990eefc6e81e82434f66a633afef892991ce3a
Alexander Bantyev <balsoft@yandex.ru>
2019-05-30 23:30:20 +0300

    Update versions of imports

---
+++ imports/update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p git -i bash
+
+IN_NIX_SHELL=
+
+imports=$(pwd)
+
+for author in `ls $imports/github`
+do
+    for repo in `ls "$imports/github/$author"`
+    do
+        cd "$imports/github/$author"
+        git init "./$repo"
+        git submodule add --force "https://github.com/$author/$repo" "./$repo"
+        git submodule init
+        cd "$repo"
+        git remote add origin "https://github.com/$author/$repo"
+        git fetch
+        git checkout master
+        git pull
+    done
+done
+
+cd $imports/nixpkgs
+git checkout nixos-unstable
+git pull
+
+cd $imports/..
+
+git add .
+git commit -m "Update versions of imports"
+
