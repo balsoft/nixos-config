88d2dbde7db38432bdede05a093e6229e1e9e447
Alexander Bantyev <balsoft@yandex.ru>
2019-05-31 16:07:21 +0300

    Automatic commit. This builds at 1559308041

--- modules/packages.nix
+++ modules/packages.nix
@@ -17,6 +17,16 @@
         ] ++ oldAttrs.patches;
       });
 
+      nerdfonts = old.stdenv.mkDerivation rec {
+        name = "RobotoMonoNerd";
+        src = old.fetchzip {
+          url = https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/RobotoMono.zip;
+          sha256 = "sha256:1i78fn62x0337p2974dn1nga1pbdi7mqg203h81yi9b79pyxv9bh";
+          stripRoot = false;
+        };
+        installPhase = "mkdir -p $out/share/fonts; cp $src/* $out/share/fonts";
+      };
+
       pythonPackages = old.pythonPackages.override {
         overrides = (self: super: {
           backports_functools_lru_cache =
