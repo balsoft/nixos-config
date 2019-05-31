a965f128d89fa1643999d9a167c5ad86d8b57d90
Alexander Bantyev <balsoft@yandex.ru>
2019-05-31 20:10:25 +0300

    Update versions of imports

--- modules/workspace/compton.nix
+++ modules/workspace/compton.nix
@@ -3,6 +3,6 @@
     programs.autorandr.hooks.preswitch.compton = "pkill compton";
     programs.autorandr.hooks.postswitch.compton = "allow_rgb10_configs=false ${
       pkgs.compton
-    }/bin/compton -c -C --backend glx --vsync --shadow-exclude '!(I3_FLOATING_WINDOW@:c = 1)' --shadow-exclude-reg 1920x24+0+0";
+    }/bin/compton -c -C --backend glx --vsync --shadow-exclude '!(I3_FLOATING_WINDOW@:c = 1)'";
   };
 }
