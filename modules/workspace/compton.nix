ef1620d802416e99c92e3185222624345231d2b0
Alexander Bantyev <balsoft@yandex.ru>
2019-06-01 10:09:55 +0300

    Automatic commit. This builds at 1559372995

--- modules/workspace/compton.nix
+++ modules/workspace/compton.nix
@@ -1,8 +1,10 @@
 { pkgs, lib, config, ... }: {
   home-manager.users.balsoft = {
     programs.autorandr.hooks.preswitch.compton = "pkill compton";
-    programs.autorandr.hooks.postswitch.compton = "allow_rgb10_configs=false ${
-      pkgs.compton
-    }/bin/compton -c -C --backend=glx --vsync=opengl --shadow-exclude='!(I3_FLOATING_WINDOW@:c = 1)'";
+    programs.autorandr.hooks.postswitch.compton = "allow_rgb10_configs=false ${pkgs.compton}/bin/compton";
+    services.compton = {
+      enable = true;
+      backend = "glx";
+    };
   };
 }
