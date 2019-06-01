2e3977c6fdad82a135f2b6bc7e5e7ff8452fd76e
Alexander Bantyev <balsoft@yandex.ru>
2019-06-01 11:32:39 +0300

    Automatic commit. This builds at 1559377959

--- modules/applications/emacs/init.el
+++ modules/applications/emacs/init.el
@@ -189,6 +189,7 @@ If point was already at that position, move point to beginning of line."
 (use-package xresources-theme
   :defer t
   :config
+  (load-theme 'xresources t)
   (add-hook 'after-make-frame-functions
             (lambda (frame)
               (with-selected-frame frame
