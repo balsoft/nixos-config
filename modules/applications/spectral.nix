940c59e12e25a6e5f8f55cebf63735ddd4c45e33
Alexander Bantyev <balsoft@yandex.ru>
2019-06-17 12:26:37 +0300

    Automatic commit. This builds at 1560763597

--- modules/applications/spectral.nix
+++ modules/applications/spectral.nix
@@ -1,15 +1,34 @@
-{ pkgs, config, lib, ... }: {
+{ pkgs, config, lib, ... }:
+with import ../../support.nix { inherit lib config; }; {
   nixpkgs.overlays = [
     (self: super: {
       spectral = super.spectral.overrideAttrs (oldAttrs: {
-        prePatch =
-        "substituteInPlace qml/main.qml --replace '#303030' '${config.themes.colors.bg}'";
+        prePatch = ''
+          substituteInPlace qml/main.qml --replace "#303030" "${config.themes.colors.bg}"
+          cp ${
+            pkgs.writeTextFile {
+              name = "qtquickcontrols2.conf";
+              text = genIni {
+                Controls.Style = "Material";
+                Material = {
+                  Theme = "Dark";
+                  Variant = "Dense";
+                  Primary = config.themes.colors.alt;
+                  Accent = config.themes.colors.gray;
+                  Background = config.themes.colors.bg;
+                  Foreground = config.themes.colors.fg;
+                };
+              };
+            }
+          } .
+        '';
       });
     })
   ];
   home-manager.users.balsoft = {
-    xsession.windowManager.i3.config.startup =
-    [{ command = "${pkgs.spectral}/bin/spectral"; }];
+    xsession.windowManager.i3.config = {
+      startup = [{ command = "${pkgs.spectral}/bin/spectral"; }];
+    };
     home.packages = [ pkgs.spectral ];
   };
 }
