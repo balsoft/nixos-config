f11cc64c30654bef3ebb26df96a7bd1dadae7098
Alexander Bantyev <balsoft@yandex.ru>
2019-06-24 12:51:34 +0300

    Automatic commit. This builds at 1561369894

--- modules/services.nix
+++ modules/services.nix
@@ -4,16 +4,25 @@
 
   services.mopidy = {
     enable = true;
-    extensionPackages = with pkgs; [mopidy-gmusic];
+
+    extensionPackages = with pkgs; [ mopidy-gmusic ];
     configuration = if (!isNull config.secrets.gpmusic) then ''
       [gmusic]
       username = ${config.secrets.gpmusic.user}
       password = ${config.secrets.gpmusic.password}
       deviceid = ${config.secrets.gpmusic.deviceid}
     '' else
-      "";
+      "" + ''
+        [mpd]
+        enabled = true
+        hostname = ::
+        port = 6600
+        password =
+        zeroconf = Mopidy MPD server on ${config.device}
+        command_blacklist = listall,listallinfo
+      '';
   };
-  
+
   systemd.services.mopidy.serviceConfig.User = lib.mkForce "balsoft";
 
   services.earlyoom = {
@@ -24,7 +33,7 @@
 
   services.printing = {
     enable = true;
-    drivers = [pkgs.gutenprint];
+    drivers = [ pkgs.gutenprint ];
   };
   programs.dconf.enable = true;
 
@@ -32,9 +41,7 @@
     enable = true;
     client.enable = true;
     client.privoxy.enable = true;
-    hiddenServices.sshThroughNAT.map = [
-      { port = 22; }
-    ];
+    hiddenServices.sshThroughNAT.map = [{ port = 22; }];
     torsocks.enable = true;
     client.socksListenAddressFaster = "0.0.0.0:9063";
   };
