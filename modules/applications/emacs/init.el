0c3771c8ffb28d7b47c84b736cefc6d96d93eb96
Alexander Bantyev <balsoft@yandex.ru>
2019-07-20 11:35:38 +0300

    Automatic commit. This builds at 1563611738

--- modules/applications/emacs/init.el
+++ modules/applications/emacs/init.el
@@ -284,6 +284,16 @@ If point was already at that position, move point to beginning of line."
 
 (use-package company-tabnine)
 
+(require 'color)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+
 (use-package yasnippet-snippets
   :defer t)
 
