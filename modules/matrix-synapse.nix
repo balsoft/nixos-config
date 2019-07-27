0f1afdf802fd38e433a3c99861597c2a7ced879f
Alexander Bantyev <balsoft@yandex.ru>
2019-07-27 14:09:43 +0300

    Automatic commit. This builds at 1564225783

--- modules/matrix-synapse.nix
+++ modules/matrix-synapse.nix
@@ -3,7 +3,7 @@
     enable = true;
     allow_guest_access = true;
     listeners = [{
-      bind_address = "0.0.0.0";
+      bind_address = "balsoft.online";
       port = 13748;
       resources = [
         {
