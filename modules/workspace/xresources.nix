73cbef63a3f546f74acdc7c4a30c78bb25a6f5a5
Alexander Bantyev <balsoft@yandex.ru>
2019-07-27 13:11:12 +0300

    Automatic commit. This builds at 1564222272

--- modules/workspace/xresources.nix
+++ modules/workspace/xresources.nix
@@ -4,6 +4,23 @@
     xresources.properties = with config.themes.colors; {
       "*background" = bg;
       "*foreground" = fg;
+      "*color0" = dark;
+      "*color1" = red;
+      "*color2" = green;
+      "*color3" = yellow;
+      "*color4" = blue;
+      "*color5" = purple;
+      "*color6" = cyan;
+      "*color7" = gray;
+      "*color8" = dark;
+      "*color9" = red;
+      "*color10" = green;
+      "*color11" = yellow;
+      "*color12" = blue;
+      "*color13" = purple;
+      "*color14" = cyan;
+      "*color15" = gray;
+      
       "emacs.color0" = dark;
       "emacs.color1" = red;
       "emacs.color2" = green;
@@ -21,23 +38,6 @@
       "emacs.color14" = cyan;
       "emacs.color15" = fg;
 
-      "*.color0" = dark;
-      "*.color1" = red;
-      "*.color2" = green;
-      "*.color3" = yellow;
-      "*.color4" = blue;
-      "*.color5" = purple;
-      "*.color6" = cyan;
-      "*.color7" = gray;
-      "*.color8" = dark;
-      "*.color9" = red;
-      "*.color10" = green;
-      "*.color11" = yellow;
-      "*.color12" = blue;
-      "*.color13" = purple;
-      "*.color14" = cyan;
-      "*.color15" = gray;
-      
     };
     home.activation.xrdb = {
       after = ["linkGeneration"];
