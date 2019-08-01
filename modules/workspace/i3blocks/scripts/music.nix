f33db6aa9029795db90c7f5196f4e9f969202f13
Alexander Bantyev <balsoft@yandex.ru>
2019-08-02 01:09:38 +0300

    Automatic commit. This builds at 1564697378

--- modules/workspace/i3blocks/scripts/music.nix
+++ modules/workspace/i3blocks/scripts/music.nix
@@ -1,21 +1,19 @@
-{ python3, playerctl, iconfont, ...}: 
-# ''
-#   #!${python3}/bin/python
-#   from subprocess import getoutput, call
-#   from os import environ
-#   status = getoutput("${playerctl}/bin/playerctl status")
-#   if "BLOCK_BUTTON" in environ:
-#     BLOCK_BUTTON = int(environ["BLOCK_BUTTON"])
-#     if BLOCK_BUTTON == 1: call(["${playerctl}/bin/playerctl", "play-pause"])
-#     if BLOCK_BUTTON == 2: call(["${playerctl}/bin/playerctl", "stop"])
-#     if BLOCK_BUTTON == 3: call(["${playerctl}/bin/playerctl", "next"])
-#   if status == "Paused": 
-#     icon=""
-#   elif status == "Playing": 
-#     icon=""
-#   else: 
-#     icon = ""
-#   text = getoutput("${playerctl}/bin/playerctl metadata title")[:20:]
-#   print("<span font='${iconfont}'>%s</span> %s" % (icon, text))
-# ''
-"${playerctl}/bin/playerctl metadata title"
+{ python3, playerctl, iconfont, ...}: ''
+  #!${python3}/bin/python
+  from subprocess import getoutput, call
+  from os import environ
+  status = getoutput("${playerctl}/bin/playerctl status")
+  if "BLOCK_BUTTON" in environ:
+    BLOCK_BUTTON = environ["BLOCK_BUTTON"]
+    if BLOCK_BUTTON == "1": call(["${playerctl}/bin/playerctl", "play-pause"])
+    if BLOCK_BUTTON == "2": call(["${playerctl}/bin/playerctl", "stop"])
+    if BLOCK_BUTTON == "3": call(["${playerctl}/bin/playerctl", "next"])
+  if status == "Paused": 
+    icon=""
+  elif status == "Playing": 
+    icon=""
+  else: 
+    icon = ""
+  text = getoutput("${playerctl}/bin/playerctl metadata title")[:20:]
+  print("<span font='${iconfont}'>%s</span> %s" % (icon, text))
+''
