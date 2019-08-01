b3afac17e03062c8a31b250b000c5c17913fe4d2
Alexander Bantyev <balsoft@yandex.ru>
2019-08-01 10:59:08 +0300

    Automatic commit. This builds at 1564646348

--- modules/workspace/zsh.nix
+++ modules/workspace/zsh.nix
@@ -11,6 +11,36 @@
       theme = "agnoster";
       plugins = [ "git" "dirhistory" ];
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+         owner = "chisui";
+         repo = "zsh-nix-shell";
+         rev = "b2609ca787803f523a18bb9f53277d0121e30389";
+         sha256 = "01w59zzdj12p4ag9yla9ycxx58pg3rah2hnnf3sw4yk95w3hlzi6";
+       };
+      }
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.4.0";
+          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+      {
+        name = "you-should-use";
+        src = pkgs.fetchFromGitHub {
+          owner = "MichaelAquilina";
+          repo = "zsh-you-should-use";
+          rev = "2be37f376c13187c445ae9534550a8a5810d4361";
+          sha256 = "0yhwn6av4q6hz9s34h4m3vdk64ly6s28xfd8ijgdbzic8qawj5p1";
+        };
+      }
+    ];
     shellAliases = {
       "b" = ''nix-build "<nixpkgs>" --no-out-link -A'';
       "p" = "nix-shell --run zsh -p";
@@ -60,15 +90,6 @@
       # make sure this plays nicely with any existing preexec
       preexec_functions+=( notifyosd-preexec )
       XDG_DATA_DIRS=$XDG_DATA_DIRS:$GSETTINGS_SCHEMAS_PATH
-
-      source ${
-       pkgs.fetchFromGitHub {
-         owner = "chisui";
-         repo = "zsh-nix-shell";
-         rev = "b2609ca787803f523a18bb9f53277d0121e30389";
-         sha256 = "01w59zzdj12p4ag9yla9ycxx58pg3rah2hnnf3sw4yk95w3hlzi6";
-       }
-     }/nix-shell.plugin.zsh
        
       PS1="$PS1
      $ "
