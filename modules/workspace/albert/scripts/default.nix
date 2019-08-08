9ab4bbc5b2f9a1f73334df0e1ec671601dc7ff69
Alexander Bantyev <balsoft@yandex.ru>
2019-08-08 10:46:55 +0300

    Automatic commit. This builds at 1565250415

--- modules/workspace/albert/scripts/default.nix
+++
@@ -1,9 +0,0 @@
-{
-    qalc = ./qalc.py;
-    nix = ./nix.py;
-    translate = ./translate.py;
-    win = ./win.py;
-    swayctrl = ./swayctrl.py;
-    termNote = ./termNote.py;
-    emacs = ./emacs.py;
-}
