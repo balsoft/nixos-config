82de80a5570f3d4ebbfe081fc6170357c991b253
Alexander Bantyev <balsoft@yandex.ru>
2019-08-16 18:10:34 +0300

    Automatic commit. This builds at 1565968234

--- modules/workspace/i3/default.nix
+++ modules/workspace/i3/default.nix
@@ -188,7 +188,7 @@ in {
         "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause";
         "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
         "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
-        "--release button2" = "kill";
+        "button2" = "kill";
         "--whole-window ${modifier}+button2" = "kill";
       } // builtins.listToAttrs (builtins.genList (x: {
         name = "${modifier}+${toString x}";
