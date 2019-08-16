730376c5b398ecaecc572902b9376f6f4b52cf42
Alexander Bantyev <balsoft@yandex.ru>
2019-08-16 15:37:01 +0300

    Automatic commit. This builds at 1565959021

--- modules/workspace/kanshi.nix
+++ modules/workspace/kanshi.nix
@@ -1,23 +1,15 @@
-{ config, pkgs, lib, ... }:
-{
-  home-manager.users.balsoft = 
-  {
-    # xsession.windowManager.i3.config.startup = 
-    # [ {command = "${pkgs.kanshi}/bin/kanshi"; notification = false; always = true;} ];                           
-    xdg.configFile."kanshi/config".text = 
-    ''
-    {
-      output eDP-1 resolution 1920x1080 position 0,853 scale 1.5
-      output HDMI-A-2 resolution 1920x1080 position 1280,853 scale 1.0
-      output HDMI-A-1 vendor DEL serial MC0435BH2ATP resolution 1280x1024 position 0,0 scale 1.2
-    }
-    {
-      output eDP-1 resolution 1920x1080 position 0,0 scale 1.0
-      output *
-    }
-    {
-      output eDP-1 resolution 1920x1080 position 0,0 scale 1.0
-    }
+{ config, pkgs, lib, ... }: {
+  home-manager.users.balsoft = {
+    xsession.windowManager.i3.config.startup = [{
+      command = "${pkgs.kanshi}/bin/kanshi";
+      notification = false;
+      always = true;
+    }];
+    xdg.configFile."kanshi/config".text = ''
+      {
+        output HDMI-A-1 resolution 1920x1080 position 1280,853 scale 1.0
+        output DVI-D-1 vendor DEL serial MC0435BH2ATP resolution 1280x1024 position 0,0 scale 1.2
+      }
     '';
   };
 }
