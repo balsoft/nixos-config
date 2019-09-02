4d0474d40a8c0440254165a5f0845e0e25476fa9
Alexander Bantyev <balsoft@yandex.ru>
2019-09-02 16:40:02 +0300

    Automatic commit. This builds at 1567431602

--- default.nix
+++ default.nix
@@ -6,21 +6,21 @@
 # To use this configuration:
 #   1. Add your own secret.nix to this folder
 #   2. Replace /etc/nixos/configuration.nix with the following:
-  #      import /path/to/this/nixos-config "Vendor-Type"
-  #   3. Log in to application and services where neccesary
-
+#      import /path/to/this/nixos-config "Vendor-Type"
+#   3. Log in to application and services where neccesary
 
 device: # This is the device we're on now
-{ config, pkgs, lib, ... }: 
-{
-  imports =
-  [
+{ config, pkgs, lib, ... }:
+let sources = import ./nix/sources.nix;
+in {
+  imports = [
     /etc/nixos/hardware-configuration.nix
-    "${(import ./nix/sources.nix).home-manager}/nixos"
+    "${sources.home-manager}/nixos"
+    "${sources.simple-nixos-mailserver}"
     ./modules
   ];
 
-  inherit device;  
+  inherit device;
 
   system.stateVersion = "18.03";
 }
