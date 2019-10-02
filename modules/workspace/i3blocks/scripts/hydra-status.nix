078d7211912aa6c1a840b9a4b663889cc782332a
Alexander Bantyev <balsoft@yandex.ru>
2019-10-02 10:10:34 +0300

    Update modules/workspace/i3blocks/scripts/hydra-status.nix

--- modules/workspace/i3blocks/scripts/hydra-status.nix
+++ modules/workspace/i3blocks/scripts/hydra-status.nix
@@ -1,7 +1,7 @@
 { bash, curl, libnotify, sox, ... }:
 ''
   #!${bash}/bin/bash
-  STATUS=$(${curl}/bin/curl -sw "%{http_code}" http://hydra.typeable.io/ping)
+  STATUS=$(${curl}/bin/curl -sw "%{http_code}" https://hydra.typeable.io/ping)
   if [ $STATUS -eq 200 ]
   then
     echo 🐍
