a2726e33304c2d913dcd342e913a969a91c8ae52
Alexander Bantyev <balsoft@yandex.ru>
2019-11-02 14:59:59 +0300

    Update modules/workspace/kanshi.nix

--- modules/workspace/kanshi.nix
+++ modules/workspace/kanshi.nix
@@ -8,7 +8,7 @@
     xdg.configFile."kanshi/config".text = ''
       {
         output "Chimei Innolux Corporation 0x1361 0x00000000" mode 1920x1080 position 0,500 scale 1.2
-        output "Unknown VZ249 G6LMRS048669" mode 1920x1080 position 1600,0 scale 1.0
+        output "Unknown VZ249 G6LMRS048669" mode 1920x1080 position 1920,0 scale 1.0
       }
     '';
   };
