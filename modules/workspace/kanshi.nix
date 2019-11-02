c16d2841b99dcd7317fb06e118d8da775a06a025
Alexander Bantyev <balsoft@yandex.ru>
2019-11-02 12:57:25 +0300

    Update modules/workspace/kanshi.nix

--- modules/workspace/kanshi.nix
+++ modules/workspace/kanshi.nix
@@ -7,8 +7,8 @@
     }];
     xdg.configFile."kanshi/config".text = ''
       {
-        output eDP-1 position 0,500 scale 1.2
-        output HDMI-A-2 serial G6LMRS048669 position 1600,0 scale 1.0
+        output "Chimei Innolux Corporation 0x1361 0x00000000" mode 1920x1080 position 0,500 scale 1.2
+        output "Unknown VZ249 G6LMRS048669" mode 1920x1080 position 1600,0 scale 1.0
       }
     '';
   };
