c68f594f06ad2e19d3dbf40599486ea79c7fb234
Alexander Bantyev <balsoft@yandex.ru>
2019-11-05 16:55:47 +0300

    t Update modules/applications/yt-utilities.nix

--- modules/applications/yt-utilities.nix
+++ modules/applications/yt-utilities.nix
@@ -2,16 +2,16 @@
 {
   
   home-manager.users.balsoft = {
-    home.file.".yt.yaml".text = ''
-      yt-token: ${config.secrets.yt-utilities.token}
-      user: ${config.secrets.yt-utilities.user}
-      slack-token: ${config.secrets.slack-term}
-      from:
-        org: "/home/balsoft/cloud/Google Drive/serokell.org"
-        full-file: true
-      since: 1997-11-19
-      severity: Debug
-    '';
+    home.file.".yt.yaml".text = builtins.toJSON {
+      yt-token = config.secrets.yt-utilities.token;
+      user = config.secrets.yt-utilities.user;
+      from = {
+        org = "/home/balsoft/cloud/Google Drive/serokell.org";
+        full-file = true;
+      };
+      since = "1997-11-19";
+      severity = "Debug";
+    };
     home.packages = [ pkgs.yt-utilities ];
   };
 }
