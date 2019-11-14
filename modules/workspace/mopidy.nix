c5c9d09ef25894e97e2ae9df056a69fff8655b45
Alexander Bantyev <balsoft75@gmail.com>
2019-11-14 13:26:24 +0300

    Update modules/workspace/mopidy.nix

--- modules/workspace/mopidy.nix
+++ modules/workspace/mopidy.nix
@@ -2,19 +2,24 @@
 {
   services.mopidy = {
     enable = true;
-    extensionPackages =
-      with pkgs; 
-      [
-        mopidy-gmusic
-      ];
-    configuration = if (! isNull config.secrets.gpmusic) then
-      ''
-        [gmusic]
-        username = ${config.secrets.gpmusic.user}
-        password = ${config.secrets.gpmusic.password}
-        deviceid = ${config.secrets.gpmusic.deviceid}
-        bitrate = 128
-      '' else "";
-
+    extensionPackages = with pkgs; [ mopidy-gmusic ];
+    configuration = (if (!isNull config.secrets.gpmusic) then ''
+      [gmusic]
+      username = ${config.secrets.gpmusic.user}
+      password = ${config.secrets.gpmusic.password}
+      deviceid = ${config.secrets.gpmusic.deviceid}
+      bitrate = 128
+    '' else
+    "") + ''
+      [mpd]
+      hostname = 0.0.0.0
+      [audio]
+      output = pulsesink server=127.0.0.1
+    '';
   };
+
+  systemd.services.mopidy = {
+    requires = [ "network-online.target" ];
+  };
+
 }
