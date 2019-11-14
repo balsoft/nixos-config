ee959d42f59d9bc71baf3a0cff8de400c513f5cb
Alexander Bantyev <balsoft75@gmail.com>
2019-11-14 13:50:07 +0300

    Remove mopidy from services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -2,29 +2,6 @@
 
   services.acpid.enable = true;
   programs.ssh.startAgent = true;
-  services.mopidy = {
-    enable = true;
-    extensionPackages = with pkgs; [ mopidy-gmusic ];
-    dataDir = "/home/balsoft/.cache/mopidy";
-    configuration = (if (!isNull config.secrets.gpmusic) then ''
-      [gmusic]
-      username = ${config.secrets.gpmusic.user}
-      password = ${config.secrets.gpmusic.password}
-      deviceid = ${config.secrets.gpmusic.deviceid}
-      bitrate = 128
-    '' else
-      "") + ''
-        [mpd]
-        hostname = 0.0.0.0
-        
-        [audio]
-        output = pulsesink server=127.0.0.1
-      '';
-  };
-
-  systemd.services.mopidy = {
-    requires = [ "network-online.target" ];
-  };
 
 
   services.earlyoom = {
