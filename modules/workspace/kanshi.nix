9183584ab8413d0e1bb21eaa55d2860cc866b081
Alexander Bantyev <balsoft75@gmail.com>
2019-11-14 13:08:30 +0300

    Update modules/workspace/kanshi.nix

--- modules/workspace/kanshi.nix
+++ modules/workspace/kanshi.nix
@@ -10,6 +10,10 @@
         output "Chimei Innolux Corporation 0x1361 0x00000000" position 0,500 scale 1.5
         output "Unknown VZ249 G6LMRS048669" position 1280,0
       }
+      {
+        output "Unknown VZ249 G6LMRS048669" position 0,1024
+        output "Dell Inc. DELL E176FP MC0435BH2ATP" position 640,0
+      }
     '';
   };
 }
