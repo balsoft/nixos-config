83994b2e03542070602b72d76b4a35ef914279ad
Alexander Bantyev <balsoft75@gmail.com>
2019-11-15 23:40:02 +0300

    Update modules/workspace/autofs.nix

--- modules/workspace/autofs.nix
+++ modules/workspace/autofs.nix
@@ -2,7 +2,7 @@
   services.autofs.enable = true;
   services.autofs.autoMaster = let
     mapConf = pkgs.writeText "auto" ''
-      ThinkPad-Laptop.lan -fstype=fuse,rw,nodev,noatime,allow_other,max_read=65536 :${pkgs.sshfs}/bin/sshfs\#balsoft@&\:
+      * -fstype=fuse,rw,nodev,noatime,allow_other,max_read=65536 :${pkgs.sshfs}/bin/sshfs\#balsoft@&\:
     '';
   in ''
     /auto file:${mapConf} uid=1000,gid=100,--timeout=30,--ghost
