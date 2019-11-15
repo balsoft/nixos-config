c3ca1aeb908df61eaecd087b171dccfcf69441d1
Alexander Bantyev <balsoft75@gmail.com>
2019-11-15 23:20:39 +0300

    Update modules/workspace/autofs.nix

--- modules/workspace/autofs.nix
+++ modules/workspace/autofs.nix
@@ -1,10 +1,8 @@
-{pkgs, config, lib, ...}:
-rec {
+{ pkgs, config, lib, ... }: rec {
   services.autofs.enable = true;
-  services.autofs.autoMaster = environment.etc."auto.master".text;
-  environment.etc."auto.master".text = let
+  services.autofs.autoMaster = let
     mapConf = pkgs.writeText "auto" ''
-      * -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536 :sshfs\#balsoft@&\:
+      balsoft.ru -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536 :sshfs\#balsoft@balsoft.ru\:
     '';
   in ''
     /auto file:${mapConf} uid=1000,gid=100,--timeout=30,--ghost
