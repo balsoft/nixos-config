1626cab52a736a41290a344b7ba1fc8a67f9f56c
Alexander Bantyev <balsoft75@gmail.com>
2019-11-17 20:25:55 +0300

    Fix music

--- modules/workspace/i3blocks/scripts/music.nix
+++ modules/workspace/i3blocks/scripts/music.nix
@@ -15,5 +15,5 @@
   else: 
     icon = ""
   text = getoutput("${playerctl}/bin/playerctl metadata title")[:20:]
-  print("<span font='${iconfont}'>%s</span> %s" % (icon, text))
+  print("<span font='${iconfont}'>%s</span> %s" % (icon, text.replace('&', '&amp;'))
 ''
