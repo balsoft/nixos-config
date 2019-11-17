67be4b7a2eefa8dbac80a7cd1a4b93139a8ffa4e
Alexander Bantyev <balsoft75@gmail.com>
2019-11-17 20:35:37 +0300

    Update modules/services.nix

--- modules/services.nix
+++ modules/services.nix
@@ -34,7 +34,5 @@
   
   services.upower.enable = true;
   virtualisation.docker.enable = config.deviceSpecific.isHost;
-  virtualisation.libvirtd = {
-    enable = config.deviceSpecific.isHost;
-  };
+  virtualisation.libvirtd.enable = config.deviceSpecific.isHost;
 }
