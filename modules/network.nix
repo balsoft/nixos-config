c3624f0eda863a142d85c2ef061d6552587d00f4
Alexander Bantyev <balsoft75@gmail.com>
2019-11-17 18:18:39 +0300

    Enable ModemManager on startup

--- modules/network.nix
+++ modules/network.nix
@@ -5,6 +5,8 @@
     usePredictableInterfaceNames = false;
     hostName = config.device;
   };
+  systemd.services.ModemManager.wantedBy =
+    lib.optional (config.device == "ThinkPad-Laptop") "network.target";
   systemd.services.dhcpcd.serviceConfig.Type = lib.mkForce
     "simple"; # TODO Make a PR with this change; forking is not acceptable for dhcpcd.
 }
