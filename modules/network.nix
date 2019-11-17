b8e3f6d096e10801b41061227d871e8c3ba1d51e
Alexander Bantyev <balsoft75@gmail.com>
2019-11-18 02:21:32 +0300

    Disable local nameserver

--- modules/network.nix
+++ modules/network.nix
@@ -2,12 +2,16 @@
   networking = {
     networkmanager.enable = true;
     firewall.enable = false;
-    resolvconf.extraConfig = "libc=NO";
+    resolvconf.extraConfig = ''
+      local_nameservers=""
+      name_server_blacklist="0.0.0.0 127.0.0.1"
+      resolv_conf_local_only=NO
+    '';
     usePredictableInterfaceNames = false;
     hostName = config.device;
   };
   systemd.services.ModemManager.wantedBy =
     lib.optional (config.device == "ThinkPad-Laptop") "network.target";
-  systemd.services.dhcpcd.serviceConfig.Type = lib.mkForce
-    "simple"; # TODO Make a PR with this change; forking is not acceptable for dhcpcd.
+  # systemd.services.dhcpcd.serviceConfig.Type = lib.mkForce
+    # "simple"; # TODO Make a PR with this change; forking is not acceptable for dhcpcd.
 }
