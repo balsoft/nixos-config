0cae3e9fc3c02eb6c44552aa72b9c3c35ed23232
Alexander Bantyev <balsoft75@gmail.com>
2019-11-29 22:00:30 +0300

    Update modules/workspace/kanshi.nix

--- modules/workspace/kanshi.nix
+++ modules/workspace/kanshi.nix
@@ -7,12 +7,8 @@
     }];
     xdg.configFile."kanshi/config".text = ''
       {
-        output "Chimei Innolux Corporation 0x1361 0x00000000" position 0,500 scale 1.5
-        output "Unknown VZ249 G6LMRS048669" position 1280,0
-      }
-      {
-        output "Unknown VZ249 G6LMRS048669" position 0,1024
-        output "Dell Inc. DELL E176FP MC0435BH2ATP" position 640,0
+        output "Samsung Electric Company C27JG5x HTOM901267" position 0,1025
+        output "Dell Inc. DELL E176FP MC0435BH2ATP" position 1000,0
       }
     '';
   };
