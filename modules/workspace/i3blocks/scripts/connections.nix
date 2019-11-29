ab29a27eca22e62987093de6a65c3e684068adde
Alexander Bantyev <balsoft75@gmail.com>
2019-11-29 12:30:51 +0300

    Allow multiple connections of the same type

--- modules/workspace/i3blocks/scripts/connections.nix
+++ modules/workspace/i3blocks/scripts/connections.nix
@@ -3,53 +3,56 @@
   [[ $BLOCK_BUTTON -eq 1 ]] && ${config.defaultApplications.term.cmd} -e ${networkmanager}/bin/nmtui-connect
   CONNECTIONS=$(${networkmanager}/bin/nmcli con show --active | tail +2 | tr -s ' ' | rev | cut -d' ' -f3 | rev)
   text=""
-  grep wifi <<< $CONNECTIONS > /dev/null && {
-    SIGNAL=$(${networkmanager}/bin/nmcli d w | grep '^\*' | tr -s ' ' | cut -d' ' -f7)
-    if [[ $SIGNAL -lt 20 ]]
-    then
-      text+=冷
-    elif [[ $SIGNAL -lt 40 ]]
-    then
-      text+=爛
-    elif [[ $SIGNAL -lt 60 ]]
-    then
-      text+=嵐
-    elif [[ $SIGNAL -lt 80 ]]
-    then
-      text+=襤
-    else
-      text+=蠟
-    fi
-  }
-  grep gsm <<< $CONNECTIONS >/dev/null && {
-    MODEM=$(${modemmanager}/bin/mmcli -K -L | tail -1 | cut -d: -f2 | tr -d ' ')
-    STATUS=$(${modemmanager}/bin/mmcli -K -m $MODEM)
-    TECH=$(grep "modem.generic.access-technologies.value\[1\]" <<< $STATUS | cut -d: -f2 | tr -d ' ')
+  for connection in $CONNECTIONS
+  do
+    grep wifi <<< $connection > /dev/null && {
+      SIGNAL=$(${networkmanager}/bin/nmcli d w | grep '^\*' | tr -s ' ' | cut -d' ' -f7)
+      if [[ $SIGNAL -lt 20 ]]
+      then
+        text+=冷
+      elif [[ $SIGNAL -lt 40 ]]
+      then
+        text+=爛
+      elif [[ $SIGNAL -lt 60 ]]
+      then
+        text+=嵐
+      elif [[ $SIGNAL -lt 80 ]]
+      then
+        text+=襤
+      else
+        text+=蠟
+      fi
+    }
+    grep gsm <<< $connection >/dev/null && {
+      MODEM=$(${modemmanager}/bin/mmcli -K -L | tail -1 | cut -d: -f2 | tr -d ' ')
+      STATUS=$(${modemmanager}/bin/mmcli -K -m $MODEM)
+      TECH=$(grep "modem.generic.access-technologies.value\[1\]" <<< $STATUS | cut -d: -f2 | tr -d ' ')
     
-    SIGNAL=$(grep "modem.generic.signal-quality.value" <<< $STATUS | cut -d: -f2 | tr -d ' ')
-    if [[ $SIGNAL -lt 20 ]]
-    then
-      text+=""
-    elif [[ $SIGNAL -lt 40 ]]
-    then
-      text+=""
-    elif [[ $SIGNAL -lt 60 ]]
-    then
-      text+=""
-    elif [[ $SIGNAL -lt 80 ]]
-    then
-      text+=""
-    else
-      text+=""
-    fi
-    if [[ $TECH == lte ]]
-    then
-      text+=""
-    else
-      text+=""
-    fi
-  }
-  grep ethernet <<< $CONNECTIONS > /dev/null && text+=""
+      SIGNAL=$(grep "modem.generic.signal-quality.value" <<< $STATUS | cut -d: -f2 | tr -d ' ')
+      if [[ $SIGNAL -lt 20 ]]
+      then
+        text+=""
+      elif [[ $SIGNAL -lt 40 ]]
+      then
+        text+=""
+      elif [[ $SIGNAL -lt 60 ]]
+      then
+        text+=""
+      elif [[ $SIGNAL -lt 80 ]]
+      then
+        text+=""
+      else
+        text+=""
+      fi
+      if [[ $TECH == lte ]]
+      then
+        text+=""
+      else
+        text+=""
+      fi
+    }
+    grep ethernet <<< $connection > /dev/null && text+=""
+  done
   code=0
   [[ $text == "" ]] && {
     text=
