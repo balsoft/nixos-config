6c048eef6b4f5f591d4be45799135978c5650571
Alexander Bantyev <balsoft75@gmail.com>
2019-12-02 18:57:28 +0300

    Update modules/workspace/email.nix

--- modules/workspace/email.nix
+++ modules/workspace/email.nix
@@ -3,11 +3,12 @@
   home-manager.users.balsoft.accounts.email.accounts."${config.secrets.gmail.user}@gmail.com" = {
     address = "${config.secrets.gmail.user}@gmail.com";
     flavor = "gmail.com";
-    passwordCommand = ''echo "${config.secrets.gmail.password}"'';
+    passwordCommand = "echo ${config.secrets.gmail.password}";
     realName = "Alexander Bantyev";
     primary = true;
     userName = config.secrets.gmail.user;
     getmail = {
+      readAll = false;
       enable = true;
       mailboxes = [ "INBOX" "Sent" ];
     };
