f2e7c8682ef75348590710c3d43006c3aa977329
Alexander Bantyev <balsoft75@gmail.com>
2019-12-09 14:22:13 +0300

    Update modules/workspace/i3blocks/scripts/youtrack-wage.nix

--- modules/workspace/i3blocks/scripts/youtrack-wage.nix
+++ modules/workspace/i3blocks/scripts/youtrack-wage.nix
@@ -4,8 +4,8 @@
 HOURS_DAY=$(yt org local --since $(date +'%Y-%m-%d') | tail -1)
 HOURS_MONTH=$(yt org local --since $(date +'%Y-%m-01') | tail -1)
 HOURS_YEAR=$(yt org local --since $(date +'%Y-01-01') | tail -1)
-MONEY_DAY=$(qalc -t -e "$HOURS_DAY * (${config.secrets.wage})")
-MONEY_MONTH=$(qalc -t -e "$HOURS_MONTH * (${config.secrets.wage})")
-MONEY_YEAR=$(qalc -t -e "$HOURS_YEAR * (${config.secrets.wage})")
+MONEY_DAY=$(qalc -s "precision 1" -t -e "$HOURS_DAY * (${config.secrets.wage})")
+MONEY_MONTH=$(qalc -s "precision 1" -t -e "$HOURS_MONTH * (${config.secrets.wage})")
+MONEY_YEAR=$(qalc -s "precision 1" -t -e "$HOURS_YEAR * (${config.secrets.wage})")
 echo "$MONEY_DAY($HOURS_DAY)/$MONEY_MONTH($HOURS_MONTH)/$MONEY_YEAR"
 ''
