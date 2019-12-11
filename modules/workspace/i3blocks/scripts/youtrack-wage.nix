5678a4eddd0f3cb29620fe318521bc4d42b67dfb
Alexander Bantyev <balsoft75@gmail.com>
2019-12-11 22:07:39 +0300

    Fix youtrack-wage

--- modules/workspace/i3blocks/scripts/youtrack-wage.nix
+++ modules/workspace/i3blocks/scripts/youtrack-wage.nix
@@ -1,6 +1,6 @@
 { bash, yt-utilities, libqalculate, config, ... }:
 ''
-#!/bin/bash
+#!${bash}/bin/bash
 HOURS_DAY=$(yt org local --since $(date +'%Y-%m-%d') | tail -1)
 HOURS_MONTH=$(yt org local --since $(date +'%Y-%m-01') | tail -1)
 HOURS_YEAR=$(yt org local --since $(date +'%Y-01-01') | tail -1)
