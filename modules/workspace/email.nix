94fc25bb40901958151923b8ec6bccb30c20188b
Alexander Bantyev <balsoft75@gmail.com>
2019-12-23 14:10:41 +0300

    Update modules/workspace/email.nix

--- modules/workspace/email.nix
+++ modules/workspace/email.nix
@@ -1,39 +1,44 @@
-{pkgs, config, ...}:
-{
-  home-manager.users.balsoft.accounts.email.accounts."${config.secrets.gmail.user}@gmail.com" = {
-    address = "${config.secrets.gmail.user}@gmail.com";
-    flavor = "gmail.com";
-    passwordCommand = "echo ${config.secrets.gmail.password}";
-    realName = "Alexander Bantyev";
-    primary = true;
-    userName = config.secrets.gmail.user;
-    getmail = {
-      readAll = false;
-      enable = true;
-      mailboxes = [ "INBOX" ];
-    };
-    msmtp = {
-      enable = true;
-    };
-    imapnotify = {
-      enable = true;
-      boxes = [ "INBOX" ];
-      onNotify = "${pkgs.libnotify}/bin/notify-send 'New mail!' '%s'";
-    };
-    signature = {
-      showSignature = "append";
-      text = ''
-        Александр Бантьев /Alexander Bantyev/ aka balsoft
-        
-        Nix, NixOS DevOPS/SRE at serokell.io
-        
-        <balsoft75@gmail.com>
-        <alexander.bantyev@serokell.io>
+{ pkgs, config, ... }: {
+  home-manager.users.balsoft = {
+    accounts.email.accounts."${config.secrets.gmail.user}@gmail.com" = {
+      address = "${config.secrets.gmail.user}@gmail.com";
+      flavor = "gmail.com";
+      passwordCommand = "echo ${config.secrets.gmail.password}";
+      realName = "Alexander Bantyev";
+      primary = true;
+      userName = config.secrets.gmail.user;
+      getmail = {
+        readAll = false;
+        enable = true;
+        mailboxes = [ "INBOX" ];
+      };
+      msmtp = { enable = true; };
+      imapnotify = {
+        enable = true;
+        boxes = [ "INBOX" ];
+        onNotify = "${pkgs.libnotify}/bin/notify-send 'New mail!' '%s'";
+      };
+      signature = {
+        showSignature = "append";
+        text = ''
+          Александр Бантьев /Alexander Bantyev/ aka balsoft
 
-        https://matrix.to/#/@balsoft:balsoft.ru
-        https://t.me/balsoft
-        https://github.com/balsoft
-      '';
+          Nix, NixOS DevOPS/SRE at serokell.io
+
+          <balsoft75@gmail.com>
+          <alexander.bantyev@serokell.io>
+
+          https://matrix.to/#/@balsoft:balsoft.ru
+          https://t.me/balsoft
+          https://github.com/balsoft
+        '';
+      };
+    };
+    services = {
+      getmail = {
+        enable = true;
+        frequency = "*-*-* *:*:00,15,30,45";
+      };
     };
   };
 }
