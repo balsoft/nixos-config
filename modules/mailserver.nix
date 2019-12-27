05a754e18639c3807ecb72964b251c75eff62764
Alexander Bantyev <balsoft@balsoft.ru>
2019-12-27 20:11:19 +0300

    Update modules/mailserver.nix

--- modules/mailserver.nix
+++ modules/mailserver.nix
@@ -1,16 +1,21 @@
 { pkgs, config, lib, ... }:
 let
   readCommandResult = command:
-  builtins.readFile (pkgs.runCommand "cmd" { preferLocalBuild = true; } "echo -n $(${command}) > $out");
-  
-  hashedPassword = readCommandResult "${pkgs.mkpasswd}/bin/mkpasswd -m sha-512 '${config.secrets.mail.password}'";
+    builtins.readFile (pkgs.runCommand "cmd" { preferLocalBuild = true; }
+      "echo -n $(${command}) > $out");
+
+  hashedPassword = readCommandResult
+    "${pkgs.mkpasswd}/bin/mkpasswd -m sha-512 '${config.secrets.mail.password}'";
 in {
   mailserver = lib.mkIf (config.device == "AMD-Workstation") {
     enable = true;
     fqdn = config.secrets.mail.host;
     domains = [ config.secrets.mail.host ];
     loginAccounts = {
-      balsoft = { inherit hashedPassword; };
+      balsoft = {
+        aliases = [ "balsoft@balsoft.ru" ];
+        inherit hashedPassword;
+      };
     };
     certificateScheme = 1;
     certificateFile = builtins.toFile "balsoft.crt" config.secrets.ssl.cert;
