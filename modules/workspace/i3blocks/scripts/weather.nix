ad42cdd49d75eae6dac5e843c72cc123c60e58ee
Alexander Bantyev <balsoft@balsoft.ru>
2020-01-02 00:01:58 +0300

    Update modules/workspace/i3blocks/scripts/weather.nix

--- modules/workspace/i3blocks/scripts/weather.nix
+++ modules/workspace/i3blocks/scripts/weather.nix
@@ -1,11 +1,10 @@
 { bash, config, curl, iconfont, ... }: ''
   #!${bash}/bin/bash
-  WEATHER=$(${curl}/bin/curl wttr.in/\?format=3 | awk -F": " '{print $2}')
-  ICON=$(echo $WEATHER | cut -d' ' -f1)
-  TEXT=$(echo $WEATHER | cut -d' ' -f2)
-  echo "<span font=\"Roboto Mono 11\">$ICON</span> $TEXT"
+  WTTR=$(curl wttr.in/?format=1)
+  echo "<span font=\"Roboto Mono 11\">$WTTR</span>"
   if [[ $BLOCK_BUTTON == 1 ]]
   then
     ${config.defaultApplications.term.cmd} --hold -e "${curl}/bin/curl wttr.in"
   fi
 ''
+ 
