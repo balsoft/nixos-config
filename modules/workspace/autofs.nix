26b142574c206669a4cbd822b20e01f1c6eca627
Alexander Bantyev <balsoft@balsoft.ru>
2020-01-07 21:08:32 +0300

    Update modules/workspace/autofs.nix

--- modules/workspace/autofs.nix
+++ modules/workspace/autofs.nix
@@ -1,6 +1,7 @@
 { pkgs, config, lib, ... }: rec {
   services.autofs.enable = true;
-  services.autofs.autoMaster = let
+  services.autofs.autoMaster = "";
+  environment.etc."autofs.conf".text = let
     mapConf = pkgs.writeText "auto" (builtins.concatStringsSep "\n" (map (n:
       "${n} -fstype=fuse,rw,nodev,noatime,allow_other,max_read=65536 :${pkgs.sshfs}/bin/sshfs#balsoft@${n}:")
       ([ "balsoft.ru" ] ++ builtins.map (n: "${n}.lan") (builtins.attrNames config.devices))));
