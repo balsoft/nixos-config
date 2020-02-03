6373f367cb4f90d135188909dfcecc92aecf64c2
Alexander Bantyev <balsoft@balsoft.ru>
2020-02-03 20:55:18 +0300

    Update modules/workspace/misc.nix

--- modules/workspace/misc.nix
+++ modules/workspace/misc.nix
@@ -37,6 +37,7 @@
       enable = true;
       userEmail = "balsoft@balsoft.ru";
       userName = "Alexander Bantyev";
+      extraConfig.pull.rebase = true;
       signing = {
         signByDefault = true;
         key = "687558B21E04FE92B255BED0E081FF12ADCB4AD5";
