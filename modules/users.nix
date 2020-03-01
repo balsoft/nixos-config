a7a740915aaf7da6a42576dab92c8b3525cc29ae
Alexander Bantyev <balsoft@balsoft.ru>
2020-03-01 21:49:01 +0300

    Update modules/users.nix

--- modules/users.nix
+++ modules/users.nix
@@ -49,7 +49,19 @@
     enable = true;
   };
 
-  security.pam.services.default.unixAuth = false;
+  security.pam.services.default = {
+    unixAuth = false;
+    text = lib.mkAfter ''
+      auth     required pam_warn.so
+      auth     required pam_deny.so
+      account  required pam_warn.so
+      account  required pam_deny.so
+      password required pam_warn.so
+      password required pam_deny.so
+      session  required pam_warn.so
+      session  required pam_deny.so
+    '';
+  };
 
   security.sudo = {
     enable = true;
