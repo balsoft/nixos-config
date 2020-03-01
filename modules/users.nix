72a158d9d405b64c69ec6b2c278aada6105df467
Alexander Bantyev <balsoft@balsoft.ru>
2020-03-01 20:49:41 +0300

    Update modules/users.nix

--- modules/users.nix
+++ modules/users.nix
@@ -27,9 +27,11 @@
 
   systemd.services."user@" = { serviceConfig = { Restart = "always"; }; };
 
-  home-manager.users.balsoft.xdg.configFile."Yubico/u2f_keys".text =
-    "balsoft:CicONGIHB4jL1NuHA36oPlPn5qsAQgIjQKGTSC3F-XZEAWdRVBX0jr8I_PxbgQR6Ys1toFJhaXtqYjY__uVhgg,04f580832926c646e918123bd516851ac5bf7d54482c1e2eb05e0e3f6aae197e4fc4853a2760794cf597001498f1833b3d36015cafa5f61703f3fbf59b73e05eca";
-
+  home-manager.users.balsoft.home.activation.yubi = {
+    data = "[ -s $XDG_CONFIG_HOME/Yubico/u2f_keys ] || pamu2fcfg > $XDG_CONFIG_HOME/Yubico/u2f_keys";
+    after = [ "linkGeneration" ];
+    before = [ ];
+  };
   home-manager.users.balsoft.home.sessionVariables.XDG_RUNTIME_DIR =
     "/run/user/1000";
 
