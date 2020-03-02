793553837edd1a3f0dd8a8ffc01d7015da2283b0
Alexander Bantyev <balsoft@balsoft.ru>
2020-03-02 16:30:34 +0300

    Update modules/mailserver.nix

--- modules/mailserver.nix
+++ modules/mailserver.nix
@@ -8,15 +8,71 @@ let
     "${pkgs.mkpasswd}/bin/mkpasswd -m sha-512 '${config.secrets.mail.password}'";
 in {
   imports = [ module ];
+  services.postfix = {
+    dnsBlacklists = [
+      "all.s5h.net"
+      "b.barracudacentral.org"
+      "bl.spamcop.net"
+      "blacklist.woody.ch"
+      "bogons.cymru.com"
+      "cbl.abuseat.org"
+      "combined.abuse.ch"
+      "db.wpbl.info"
+      "dnsbl-1.uceprotect.net"
+      "dnsbl-2.uceprotect.net"
+      "dnsbl-3.uceprotect.net"
+      "dnsbl.anticaptcha.net"
+      "dnsbl.dronebl.org"
+      "dnsbl.inps.de"
+      "dnsbl.sorbs.net"
+      "dnsbl.spfbl.net"
+      "drone.abuse.ch"
+      "duinv.aupads.org"
+      "dul.dnsbl.sorbs.net"
+      "dyna.spamrats.com"
+      "dynip.rothen.com"
+      "http.dnsbl.sorbs.net"
+      "ips.backscatterer.org"
+      "ix.dnsbl.manitu.net"
+      "korea.services.net"
+      "misc.dnsbl.sorbs.net"
+      "noptr.spamrats.com"
+      "orvedb.aupads.org"
+      "pbl.spamhaus.org"
+      "proxy.bl.gweep.ca"
+      "psbl.surriel.com"
+      "relays.bl.gweep.ca"
+      "relays.nether.net"
+      "sbl.spamhaus.org"
+      "singular.ttk.pte.hu"
+      "smtp.dnsbl.sorbs.net"
+      "socks.dnsbl.sorbs.net"
+      "spam.abuse.ch"
+      "spam.dnsbl.anonmails.de"
+      "spam.dnsbl.sorbs.net"
+      "spam.spamrats.com"
+      "spambot.bls.digibase.ca"
+      "spamrbl.imp.ch"
+      "spamsources.fabel.dk"
+      "ubl.lashback.com"
+      "ubl.unsubscore.com"
+      "virus.rbl.jp"
+      "web.dnsbl.sorbs.net"
+      "wormrbl.imp.ch"
+      "xbl.spamhaus.org"
+      "z.mailspike.net"
+      "zen.spamhaus.org"
+      "zombie.dnsbl.sorbs.net"
+    ];
+  };
   mailserver = {
     enable = true;
     fqdn = config.secrets.mail.host;
     domains = [ config.secrets.mail.host ];
     loginAccounts = {
       "balsoft@balsoft.ru" = {
-        aliases = [ "balsoft"
-        "admin@balsoft.ru" "admin"
-        "root@balsoft.ru" "root" ];
+        aliases =
+          [ "balsoft" "admin@balsoft.ru" "admin" "root@balsoft.ru" "root" ];
         inherit hashedPassword;
       };
     };
