4024f7e86c6e20e59c54a0e06d89e4ec9c7a8c0c
Alexander Bantyev <balsoft@balsoft.ru>
2020-03-22 13:33:09 +0300

    Update modules/nginx.nix

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -31,9 +31,6 @@
       "matrix.balsoft.ru" = {
         locations."/" = { proxyPass = "https://localhost:13748"; };
       } // default;
-      "mai.balsoft.ru" = {
-        locations."/" = { root = "/var/lib/important"; };
-      } // default;
       "share.balsoft.ru" = {
         locations."/" = { root = "/var/lib/share"; };
       } // default;
