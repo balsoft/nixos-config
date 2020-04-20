62a90066b4de53f048f8c820b8cf29610ab6d45e
Alexander Bantyev <balsoft@balsoft.ru>
2020-04-21 01:30:04 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -4,9 +4,10 @@
 #
 # This is main nixos configuration
 # To use this configuration:
-#   1. Add your own secret.nix to this folder
-#   2. ./install or ./bootstrap
-#   3. Log in to application and services where neccesary
+#   1. Add your own secret.nix and hardware-configuration/`hostname`.nix to this folder
+#   2. Set the hostname to the desired one
+#   3. ./install or ./bootstrap
+#   4. Log in to application and services where neccesary
 
 { config, pkgs, lib, ... }:
 let sources = import ./nix/sources.nix;
