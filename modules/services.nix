d93277c9336cdacfe9b1b86cbdcfc6069175484f
Alexander Bantyev <balsoft@balsoft.ru>
2020-08-04 15:13:03 +0300

    Move nextcloud to separate file

--- modules/services.nix
+++ modules/services.nix
@@ -50,22 +50,6 @@
   };
   networking.firewall.trustedInterfaces = [ "eth0" ];
 
-  services.nextcloud = {
-    enable = config.device == "AMD-Workstation";
-    nginx.enable = true;
-    hostName = "nextcloud.balsoft.ru";
-    config.adminpassFile = "/home/balsoft/nextcloud-admin";
-    package = pkgs.nextcloud19;
-    https = true;
-  };
-
   services.nginx.enable = config.device == "AMD-Workstation";
 
-  services.nginx.virtualHosts."nextcloud.balsoft.ru" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/".proxyWebsockets = true;
-    locations."~ ^\\/(?:build|tests|config|lib|3rdparty|templates|data)\\/".proxyWebsockets = true;
-    locations."~ ^\\/(?:index|remote|public|cron|core/ajax\\/update|status|ocs\\/v[12]|updater\\/.+|ocs-provider\\/.+|ocm-provider\\/.+)\\.php(?:$|\\/)".proxyWebsockets = true;
-  };
 }
