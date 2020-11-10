c1fc5d0285eee6832dbe7317358d441c615ffb4b
Alexander Bantyev <balsoft@balsoft.ru>
2020-11-10 14:24:32 +0300

    Fix fractional scaling

--- modules/workspace/sway/default.nix
+++ modules/workspace/sway/default.nix
@@ -233,6 +233,7 @@ in {
           transform = "90";
           position = "2560 0";
         };
+      } // lib.optionalAttrs (config.device == "X2100-Laptop") {
         "Unknown 0x0000 0x00000000".scale = "2";
       };
     };
