32803f5c8fc6459ddf3f47d318199a99e8211c89
Alexander Bantyev <balsoft@balsoft.ru>
2020-11-19 13:40:03 +0300

    Fix lambda-launcher

--- modules/packages.nix
+++ modules/packages.nix
@@ -10,33 +10,40 @@ let
 in {
   nixpkgs.overlays = [
     inputs.nix.overlay
-    (self: super:
-      rec {
-        nix = super.nix // {
-          meta = super.nix.meta // { platforms = lib.platforms.unix; };
-        };
+    (self: super: rec {
+      nix = super.nix // {
+        meta = super.nix.meta // { platforms = lib.platforms.unix; };
+      };
 
-        nur = (import inputs.NUR {
-          pkgs = old;
-          nurpkgs = pkgs;
-        }).repos;
+      nur = (import inputs.NUR {
+        pkgs = old;
+        nurpkgs = pkgs;
+      }).repos;
 
-        inherit (nur.balsoft.pkgs) termNote nix-patch;
+      inherit (nur.balsoft.pkgs) termNote nix-patch;
 
-        # inherit (old) mautrix-telegram;
+      # inherit (old) mautrix-telegram;
 
-        inherit (inputs.lambda-launcher.packages.x86_64-linux) lambda-launcher;
+      lambda-launcher = let
+        pkgs' =
+          inputs.lambda-launcher.inputs.nixpkgs.legacyPackages.x86_64-linux.extend (_: _: {
+            inherit (pkgs) glibc stdenv glibc-locales;
+          });
+      in pkgs'.callPackage "${inputs.lambda-launcher}/wrapper.nix" {
+        lambda-launcher-unwrapped =
+          pkgs'.haskellPackages.callPackage "${inputs.lambda-launcher}/lambda-launcher.nix" { };
+      };
 
-        simple-osd = inputs.simple-osd-daemons.packages.x86_64-linux;
+      simple-osd = inputs.simple-osd-daemons.packages.x86_64-linux;
 
-        inherit old;
+      inherit old;
 
-        yt-utilities = inputs.yt-utilities.defaultPackage.x86_64-linux;
+      yt-utilities = inputs.yt-utilities.defaultPackage.x86_64-linux;
 
-        nerdfonts = nur.balsoft.pkgs.roboto-mono-nerd;
+      nerdfonts = nur.balsoft.pkgs.roboto-mono-nerd;
 
-        # inherit (inputs.nixpkgs-mesa.legacyPackages.x86_64-linux) sway mesa_drivers;
-      })
+      # inherit (inputs.nixpkgs-mesa.legacyPackages.x86_64-linux) sway mesa_drivers;
+    })
   ];
   nixpkgs.config = {
     allowUnfree = true;
@@ -45,9 +52,7 @@ in {
   };
   environment.etc.nixpkgs.source = inputs.nixpkgs;
   nix = rec {
-    nixPath = lib.mkForce [
-      "nixpkgs=/etc/nixpkgs"
-    ];
+    nixPath = lib.mkForce [ "nixpkgs=/etc/nixpkgs" ];
     binaryCaches = [ "https://cache.nixos.org" ];
 
     registry.self.flake = inputs.self;
