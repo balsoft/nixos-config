8b4e7fff64b771f085e1b6d5d5cf83dec2dd6533
Alexander Bantyev <balsoft@balsoft.ru>
2021-05-13 00:45:44 +0300

    Yeet weechat config

--- modules/applications/weechat.nix
+++ modules/applications/weechat.nix
@@ -16,44 +16,6 @@ in {
     home.file.".weechat/python/autoload/go.py".source =
       "${inputs.weechat-scripts}/python/go.py";
 
-    home.file.".weechat/plugins.conf".text = ''
-      [var]
-      python.slack.auto_open_threads = "false"
-      python.slack.background_load_all_history = "true"
-      python.slack.channel_name_typing_indicator = "true"
-      python.slack.color_buflist_muted_channels = "darkgray"
-      python.slack.color_edited_suffix = "095"
-      python.slack.color_reaction_suffix = "darkgray"
-      python.slack.color_thread_suffix = "lightcyan"
-      python.slack.colorize_private_chats = "true"
-      python.slack.debug_level = "3"
-      python.slack.debug_mode = "false"
-      python.slack.distracting_channels = ""
-      python.slack.external_user_suffix = "*"
-      python.slack.files_download_location = "/home/balsoft/Downloads/slack"
-      python.slack.group_name_prefix = "&"
-      python.slack.map_underline_to = "_"
-      python.slack.migrated = "true"
-      python.slack.muted_channels_activity = "personal_highlights"
-      python.slack.never_away = "false"
-      python.slack.notify_usergroup_handle_updated = "false"
-      python.slack.record_events = "false"
-      python.slack.render_bold_as = "bold"
-      python.slack.render_italic_as = "italic"
-      python.slack.send_typing_notice = "true"
-      python.slack.server_aliases = ""
-      python.slack.shared_name_prefix = "%"
-      python.slack.short_buffer_names = "false"
-      python.slack.show_buflist_presence = "true"
-      python.slack.show_reaction_nicks = "true"
-      python.slack.slack_timeout = "20000"
-      python.slack.switch_buffer_on_join = "true"
-      python.slack.thread_messages_in_channel = "false"
-      python.slack.unfurl_auto_link_display = "both"
-      python.slack.unfurl_ignore_alt_text = "false"
-      python.slack.unhide_buffers_with_activity = "false"
-    '';
-
     home.file.".weechat/weechat.conf".text = ''
       #
       # weechat -- weechat.conf
