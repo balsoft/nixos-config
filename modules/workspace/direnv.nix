e9b90eb33b9a09e205c091cf1812db66f2c77a0b
Alexander Bantyev <balsoft@balsoft.ru>
2021-06-16 16:51:03 +0300

    Fix nix-direnv to use flakes

--- modules/workspace/direnv.nix
+++ modules/workspace/direnv.nix
@@ -1,7 +1,12 @@
 { config, pkgs, lib, ... }: {
   home-manager.users.balsoft = {
-    programs.direnv.enable = true;
-    programs.direnv.enableNixDirenvIntegration = true;
+    programs.direnv = {
+      enable = true;
+      nix-direnv = {
+        enable = true;
+        enableFlakes = true;
+      };
+    };
   };
   persist.state.directories =
     [ "/home/balsoft/.local/share/direnv" ];
