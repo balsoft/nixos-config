c52720407131f08e50e9d2166f8f5e8b9fcff8b1
Alexander Bantyev <balsoft@balsoft.ru>
2021-07-11 16:13:02 +0300

    Disable telemetry in firefox

--- profiles/applications/firefox.nix
+++ profiles/applications/firefox.nix
@@ -69,6 +69,26 @@ in {
 
           "browser.newtabpage.enabled" = false;
           "browser.startup.homepage" = "about:blank";
+
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+
+
+          "experiments.activeExperiment" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
+          "network.allow-experiments" = false;
         };
       };
       extensions = with pkgs.nur.rycee.firefox-addons; [
