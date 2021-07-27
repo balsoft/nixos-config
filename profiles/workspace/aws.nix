08942bab8476324b4af54f00a48d8d14d6492d07
Alexander Bantyev <balsoft@balsoft.ru>
2021-07-27 16:11:30 +0300

    Add AWS creds for root

--- profiles/workspace/aws.nix
+++ profiles/workspace/aws.nix
@@ -4,6 +4,8 @@
     services = [ ];
   };
 
+  systemd.tmpfiles.rules = [ "L+ /root/.aws/credentials - - - - ${config.secrets.aws_credentials.decrypted}" ];
+
   environment.sessionVariables = {
     AWS_SHARED_CREDENTIALS_FILE = config.secrets.aws_credentials.decrypted;
     AWS_CONFIG_FILE = toString (pkgs.writeText "aws_config"
