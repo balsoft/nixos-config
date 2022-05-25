29325afae6ab088f08a670c7ff0fe7bc74ac1f89
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-25 11:18:01 +0400

    Add some default gitignores

--- profiles/workspace/git.nix
+++ profiles/workspace/git.nix
@@ -4,7 +4,7 @@
     userEmail = "balsoft@balsoft.ru";
     userName = "Alexander Bantyev";
     extraConfig.pull.rebase = true;
-    ignores = [ ".envrc" ".direnv" "*~" ".#*" "#*#" ];
+    ignores = [ ".envrc" ".direnv" ".shell.nix" ".balsoft" "*~" ".#*" "#*#" ];
     signing = {
       signByDefault = true;
       key = "687558B21E04FE92B255BED0E081FF12ADCB4AD5";
