1d70f180c64d3416666790fd29b584aadbb24da6
Alexander Bantyev <balsoft@balsoft.ru>
2022-06-07 11:16:17 +0400

    Remove unused vscode ext

--- profiles/applications/vscodium/extensions.nix
+++ profiles/applications/vscodium/extensions.nix
@@ -7,12 +7,4 @@
       sha256 = "sha256-WopWzMCtiiLrx3pHNiDMZYFdjS359vu3T+6uI5A+Nv4=";
     };
   };
-  phoityne.phoityne-vscode = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      publisher = "phoityne";
-      name = "phoityne-vscode";
-      version = "0.0.27";
-      sha256 = "sha256-b+my38g6CXhSJn0c3zmZYR9AxpNvFn+gFSQ1FpznHnY=";
-    };
-  };
 }
