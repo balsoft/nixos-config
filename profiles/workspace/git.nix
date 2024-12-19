13e8959ad7b65613231ce96ac7e5e42618cda5e1
Alexander Bantyev <balsoft@balsoft.ru>
2024-12-20 00:06:57 +0400

    Add lazygit

--- profiles/workspace/git.nix
+++ profiles/workspace/git.nix
@@ -1,13 +1,19 @@
 {
-  home-manager.users.balsoft.programs.git = {
-    enable = true;
-    userEmail = "balsoft@balsoft.ru";
-    userName = "Alexander Bantyev";
-    extraConfig.pull.rebase = true;
-    ignores = [ ".envrc" ".direnv" ".shell.nix" ".balsoft" "*~" ".#*" "#*#" ];
-    signing = {
-      signByDefault = true;
-      key = "687558B21E04FE92B255BED0E081FF12ADCB4AD5";
+  home-manager.users.balsoft.programs = {
+    git = {
+      enable = true;
+      userEmail = "balsoft@balsoft.ru";
+      userName = "Alexander Bantyev";
+      extraConfig.pull.rebase = true;
+      ignores = [ ".envrc" ".direnv" ".shell.nix" ".balsoft" "*~" ".#*" "#*#" ];
+      signing = {
+        signByDefault = true;
+        key = "687558B21E04FE92B255BED0E081FF12ADCB4AD5";
+      };
+    };
+    lazygit = {
+      enable = true;
+      settings = { update.method = "never"; };
     };
   };
 }
