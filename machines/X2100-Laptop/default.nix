78a5ca1d959fcacace07003be70548473d26aea6
Alexander Bantyev <balsoft@balsoft.ru>
2025-01-13 12:01:17 +0400

    Add hibernation for X2100-Laptop

--- machines/X2100-Laptop/default.nix
+++ machines/X2100-Laptop/default.nix
@@ -1,4 +1,11 @@
-{ inputs, lib, config, pkgs, ... }: {
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
   imports = [
     ./hardware-configuration.nix
     inputs.self.nixosRoles.desktop
@@ -16,16 +23,30 @@
     };
     ram = 16;
   };
-  
-  fileSystems."/persist" =
-    { device = "/dev/disk/by-uuid/e50bd1d6-3613-465e-895a-9dde6ffaad46";
-      fsType = "ext4";
-      neededForBoot = true;
-    };
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/621A-6414";
-      fsType = "vfat";
-    };
+
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-uuid/e50bd1d6-3613-465e-895a-9dde6ffaad46";
+    fsType = "ext4";
+    neededForBoot = true;
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/621A-6414";
+    fsType = "vfat";
+  };
+
+  boot.resumeDevice = config.fileSystems."/persist".device;
+  boot.kernelParams = [ "resume_offset=4294656" ]; # from sudo filefrag -v /persist/swapfile
+  services.logind.lidSwitch = "hibernate";
+  services.logind.powerKey = "hibernate";
+
+  # boot.initrd.systemd.enable = true;
+
+  swapDevices = [
+    {
+      device = "/persist/swapfile";
+      size = 16 * 1024; # 16 GB
+    }
+  ];
 
   persist = {
     enable = true;
@@ -34,11 +55,10 @@
 
   nix.settings.max-jobs = lib.mkDefault 8;
 
-  home-manager.users.balsoft.xdg.configFile."simple-osd/brightness".text =
-    pkgs.my-lib.genIni {
-      default = {
-        "backlight backend" = "/sys/class/backlight/acpi_video0";
-        "refresh interval" = 1000;
-      };
+  home-manager.users.balsoft.xdg.configFile."simple-osd/brightness".text = pkgs.my-lib.genIni {
+    default = {
+      "backlight backend" = "/sys/class/backlight/acpi_video0";
+      "refresh interval" = 1000;
     };
+  };
 }
